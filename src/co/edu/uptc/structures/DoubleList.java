5bb1282fb2bc15f6d8eb54ab6800596f1c4c5798
BrayamReina <brayamreina2@gmail.com>
2026-02-23 16:46:50 -0500

    Metodo Contains corregido

--- src/co/edu/uptc/structures/DoubleList.java
+++ src/co/edu/uptc/structures/DoubleList.java
@@ -33,7 +33,19 @@ public class DoubleList<T> implements List<T> {
 
 	@Override
 	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
+		if (o.equals(null)) {
+			throw new NullPointerException("La lista no permite datos nulos");
+		}
+		if (o != null && !(o.getClass().equals(head.getNext().getClass()))) {
+			throw new ClassCastException("Tipo de dato incorrecto");
+		}
+		Node<T> auxNode = head;
+		for (int i = 0; i < size; i++) {
+			if (auxNode.getNext().equals(o)) {
+				return true;
+			}
+			auxNode = auxNode.getNext();
+		}
 		return false;
 	}
 
